1450d3d9b4b5e6395cd68b8155bdd5141bfc2244
Aerolix_school <79605302+Aerolixdev@users.noreply.github.com>
2022-09-09 10:02:57 -0500

    s

--- sound.js
+++ sound.js
@@ -1,14 +1,14 @@
 window.onload = (event) => {
  var random = Math.floor(Math.random() * 3) + 1;
- if (random === 1) {
+ if (random == 1) {
    var sound = document.getElementById("1");
    sound.play();
  };
- if (random === 2) {
+ if (random == 2) {
    var sound = document.getElementById("2");
    sound.play();
  };
- if (random === 3) {
+ if (random == 3) {
    var sound = document.getElementById("3");
    sound.play();
  };
